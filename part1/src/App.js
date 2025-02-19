7286ab3a90daea298a9e4501fc357bc21185836b
Lucas Solhaune <lu.solhaune@gmail.com>
2025-02-19 07:10:20 -0300

    Actualizacion del curso

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,15 +1,19 @@
+//Componente App. NUNCA crear componentes dentro de otro componente
 const App = () => {
   const course = 'Half Stack application development'
+  const title1 = 'Retomando bootcamp midudev'
   const part1 = 'Fundamentals of React'
   const exercises1 = 10
   const part2 = 'Using props to pass data'
   const exercises2 = 7
   const part3 = 'State of a component'
   const exercises3 = 14
+  
   //EJERCICIO DE PRUEBA
   return (
     <div>
       <h1>{course}</h1>
+      <h2>{title1}</h2>
       <p>
         {part1} {exercises1}
       </p>
--- part1/src/Mensaje.js
+++ part1/src/Mensaje.js
@@ -1,4 +1,4 @@
-const Mensaje = (props) =>{
+const Mensaje = (props) =>{ //Props en React = a Parametros
         
     return <h1 style={{ color: props.color }}>
         {props.message}
